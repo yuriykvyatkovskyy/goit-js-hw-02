74b887580e70594b8f1b0175bdbdefb5e8c5c518
yuriykvyatkovskyy <yuriykreik20021997@gmail.com>
2023-10-29 07:48:59 +0200

    task-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,8 +1,15 @@
 function formatMessage(message, maxLength) {
-    if (message) {
-        
+    if (maxLength >= message.length) {
+        return message;
     } else {
-        
+        return message.slice(0, maxLength) + "..."
     }
     
 }
+
+console.log(formatMessage("Curabitur ligula sapien", 16));
+console.log(formatMessage("Curabitur ligula sapien", 23));
+console.log(formatMessage("Vestibulum facilisis purus nec", 20));
+console.log(formatMessage("Vestibulum facilisis purus nec", 30));
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15));
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41));
